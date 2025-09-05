8d3619155243b4678732491069983f29968e9c0b
PC <PC@KHATTAB>
2025-09-05 19:57:33 +0300

    Testing creating WebPage Object Models.

---
+++ src/test/java/Part2/com/saucedemo/tests/products/ProductsTest.java
@@ -0,0 +1,17 @@
+package Part2.com.saucedemo.tests.products;
+
+import Part2.com.saucedemo.base.BaseTest;
+import com.saucedemo.pages.ProductsPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ProductsTest extends BaseTest {
+
+    @Test
+    public void testProductsHeaderIsDisplayed()
+    {
+        ProductsPage productsPage = loginPage.
+                logIntoApplication("standard_user","secret_sauce");
+        Assert.assertTrue(productsPage.isProductsHeaderDisplayed(),"\n Products Header is not Displayed \n");
+    }
+}
